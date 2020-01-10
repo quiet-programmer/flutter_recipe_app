530a41c2b919c3d9ef37aca573a8236e222099d5
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-01-10 20:20:57 +0100

    updated tabs screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import './screens/tabs_screen.dart';
-import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
 
